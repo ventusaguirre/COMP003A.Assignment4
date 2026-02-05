9716c4700d9a7ea907be34581ef5a0488d405eee
ventusaguirre <ventusaguirre@my.whccd.edu>
2026-02-05 14:26:36 -0800

    added program "menu" and initialized counter

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace COMP003A.Assignment4
+﻿using System.Diagnostics.Metrics;
+
+namespace COMP003A.Assignment4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1. Start/Continue Routine\n2. View Routine Progress\n3. Exit\n\nEnter choice: ");
+            
+            int counter = 1;
+          
+            do
+            {
+                Console.ReadLine();
+
+                switch (counter) { }
+            }
+            while (counter <=5);
+           
         }
     }
 }
