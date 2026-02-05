bee964c4467fe1aad84032499fde0747099df901
ventusaguirre <ventusaguirre@my.whccd.edu>
2026-02-05 15:09:16 -0800

    added switch statement and "case 1"

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -8,13 +8,20 @@ namespace COMP003A.Assignment4
         {
             Console.WriteLine("1. Start/Continue Routine\n2. View Routine Progress\n3. Exit\n\nEnter choice: ");
             
-            int counter = 1;
+            int counter = 0;
+
+            string userInput = Console.ReadLine();
           
             do
             {
-                Console.ReadLine();
 
-                switch (counter) { }
+                switch (userInput) 
+                {
+                    case "1": 
+                        counter++;
+                        Console.WriteLine($"{userInput}\nStep {counter} completed.");
+                            break;
+                }
             }
             while (counter <=5);
            
