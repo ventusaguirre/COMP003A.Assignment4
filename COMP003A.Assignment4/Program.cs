c89bef0eafd06082bd2be5bfbe9011637956247b
ventusaguirre <ventusaguirre@my.whccd.edu>
2026-02-05 19:37:26 -0800

    added case 2, 3, & default to switch statement

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -6,24 +6,45 @@ namespace COMP003A.Assignment4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. Start/Continue Routine\n2. View Routine Progress\n3. Exit\n\nEnter choice: ");
+            Console.WriteLine("1. Start/Continue Routine\n2. View Routine Progress\n3. Exit");
             
             int counter = 0;
 
-            string userInput = Console.ReadLine();
-          
             do
             {
+                Console.Write("\nEnter choice: ");
+
+                string userInput = Console.ReadLine();
 
                 switch (userInput) 
                 {
+                    case "3":
+                        Console.WriteLine("Program ended.");
+                        break;
+
                     case "1": 
                         counter++;
-                        Console.WriteLine($"{userInput}\nStep {counter} completed.");
+                        Console.WriteLine($"Step {counter} completed.");
                             break;
+
+                    case "2":
+                        Console.WriteLine($"Current progress: {counter} steps completed.");
+                        break;
+
+
+                    default: Console.WriteLine("Invalid input");
+                        break;
                 }
+
+                if (counter == 3)
+                    Console.WriteLine("Step 3 is restricted and was skipped.");
+
+                if (counter == 5)
+                    Console.WriteLine("The program has ended for safety reasons.");
             }
-            while (counter <=5);
+            while (counter <=4);
+
+            
            
         }
     }
