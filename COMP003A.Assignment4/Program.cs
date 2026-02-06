8a597f3e89809ef49ddb75fca14195484b5d0c3f
ventusaguirre <ventusaguirre@my.whccd.edu>
2026-02-05 20:30:01 -0800

    added comments to explain each case and if statement within switch function

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -8,26 +8,27 @@ namespace COMP003A.Assignment4
         {
             Console.WriteLine("1. Start/Continue Routine\n2. View Routine Progress\n3. Exit");
             
-            int counter = 0;
+            int counter = 0; 
 
             while (counter <=5)
             {
-                Console.Write("\nEnter choice: ");
+                Console.Write("\nEnter choice: "); 
 
                 string userInput = Console.ReadLine();
 
                 switch (userInput) 
                 {
-                    case "1": 
+                    
+                    case "1": //increases counter by one and outputs which step was completed
                         counter++;
 
-                        if (counter == 3)
+                        if (counter == 3) //skips "step 3" and lets user know why it was skipped
                         {
                             Console.WriteLine("Step 3 is restricted and was skipped.");
                             continue;
                         }
 
-                        if (counter >= 5)
+                        if (counter >= 5) //ends the routine if the user attempts to continue after step 4
                         {
                             Console.WriteLine("The routine has ended for safety reasons.");
                             continue;
@@ -36,11 +37,11 @@ namespace COMP003A.Assignment4
                         Console.WriteLine($"Step {counter} completed.");
                             break;
 
-                    case "2":
+                    case "2": //displays how many steps have been completed
                         Console.WriteLine($"Current progress: {counter} steps completed.");
                         break;
 
-                    case "3":
+                    case "3": //ends the loop and exits the program
                         Console.WriteLine("Program ended.");
                         Environment.Exit(0);
                         break;
